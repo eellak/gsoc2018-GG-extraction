8d1b9b68066da1875e201f571125c3d5597e4b87
ChrisKK <ckarageorgkaneen@gmail.com>
2018-05-11 14:32:43 +0300

    Update pdf to text testing

--- src/tests/context.py
+++ src/tests/context.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import sys
+from subprocess import call
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # import main
--- src/tests/test_pdf_parser.py
+++ src/tests/test_pdf_parser.py
@@ -1,14 +1,40 @@
-from context import submodules, unittest
+from context import submodules, unittest, call, os
 from submodules.pdf_parser import PDFParser
 
 
 class PDFParserTest(unittest.TestCase):
 	
-	parser = PDFParser()
+	def setUp(self):
+		self.parser = PDFParser()
+		self.test_pdfs_dir = '/data/test_PDFs/'
+		self.test_txts_dir = '/data/test_TXTs/'
 
-	def test_pdf_to_txt_for_all_in_data_dir(self):
-		self.parser.pdf_to_txt()
+	def tearDown(self): 
+		
+		print(os.getcwd())
 
+		# Remove all test texts
+		txt_list = os.listdir('..' + self.test_txts_dir)
+		for txt in txt_list: 
+			os.remove('..' + self.test_txts_dir + txt)
+
+	def test_pdf_to_txt(self): 
+		
+		texts = []
+		
+		texts.append(self.parser.get_pdf_text('ΦΕΚ A 1 - 12.01.2016.pdf', 
+											  self.test_pdfs_dir, 
+											  self.test_txts_dir))
+
+		texts.append(self.parser.get_pdf_text('ΦΕΚ A 12 - 01.02.2016.pdf', 
+											 self.test_pdfs_dir, 
+										     self.test_txts_dir))
+
+		texts.append(self.parser.get_pdf_text('ΦΕΚ A 35 - 02.03.2016.pdf', 
+											 self.test_pdfs_dir, 
+										     self.test_txts_dir))
+		
+		self.assertTrue(all(text is not None for text in texts))
 
 if __name__ == '__main__':
 	unittest.main()
