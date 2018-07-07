d70111f2b11ee9ab764e37541a687001835a70bc
ChrisKK <ckarageorgkaneen@gmail.com>
2018-07-08 00:44:42 +0300

    Fetch all rough paorg respa sections

--- src/tests/test_parser.py
+++ src/tests/test_parser.py
@@ -1308,20 +1308,95 @@ class ParserTest(unittest.TestCase):
 		txt_4 = self.get_txt('4_Pres_Decree', pdf_path=ref_respa_pdf_path)
 		txt_5 = self.get_txt('5_Pres_Decree', pdf_path=ref_respa_pdf_path)
 
-		rough_paorg_respa_sections_1 = self.parser.get_rough_respas_of_organization_units_from_pres_decree_txt(txt_1)
-		print(len(rough_paorg_respa_sections_1))
+		## 
+		#  Decision Contents
+		##
+		dec_contents_1 = self.parser.get_dec_contents_from_txt(txt_1);
+		dec_contents_2 = self.parser.get_dec_contents_from_txt(txt_2); 
+		dec_contents_3 = self.parser.get_dec_contents_from_txt(txt_3); 
+		dec_contents_4 = self.parser.get_dec_contents_from_txt(txt_4);
+		dec_contents_5 = self.parser.get_dec_contents_from_txt(txt_5);
 		
-		rough_paorg_respa_sections_2 = self.parser.get_rough_respas_of_organization_units_from_pres_decree_txt(txt_2)
-		print(len(rough_paorg_respa_sections_2))
+		self.assertTrue(not dec_contents_1);
+		self.assertTrue(not dec_contents_2); 
+		self.assertTrue(not dec_contents_3); 
+		self.assertTrue(not dec_contents_4);
+		self.assertTrue(not dec_contents_5);
+		
+		## 
+		#  Decision Summaries
+		## 
+		dec_summaries_1 = self.parser.get_dec_summaries_from_txt(txt_1, dec_contents_1);
+		dec_summaries_2 = self.parser.get_dec_summaries_from_txt(txt_2, dec_contents_2);
+		dec_summaries_3 = self.parser.get_dec_summaries_from_txt(txt_3, dec_contents_3);
+		dec_summaries_4 = self.parser.get_dec_summaries_from_txt(txt_4, dec_contents_4);
+		dec_summaries_5 = self.parser.get_dec_summaries_from_txt(txt_5, dec_contents_5);
+		print(dec_summaries_1)
+		print(dec_summaries_2)
+		print(dec_summaries_3)
+		print(dec_summaries_4)
+		print(dec_summaries_5)
 
-		rough_paorg_respa_sections_3 = self.parser.get_rough_respas_of_organization_units_from_pres_decree_txt(txt_3)
-		print(len(rough_paorg_respa_sections_3))
+		self.assertTrue(dec_summaries_1);
+		self.assertTrue(dec_summaries_2); 
+		self.assertTrue(dec_summaries_3); 
+		self.assertTrue(dec_summaries_4);
+		self.assertTrue(dec_summaries_5);
 
-		rough_paorg_respa_sections_4 = self.parser.get_rough_respas_of_organization_units_from_pres_decree_txt(txt_4)
-		print(rough_paorg_respa_sections_4)
+		## 
+		#  Decisions
+		##
+		decisions_1 = self.parser.get_decisions_from_txt(txt_1, len(dec_summaries_1))
+		decisions_2 = self.parser.get_decisions_from_txt(txt_2, len(dec_summaries_2))
+		decisions_3 = self.parser.get_decisions_from_txt(txt_3, len(dec_summaries_3))
+		decisions_4 = self.parser.get_decisions_from_txt(txt_4, len(dec_summaries_4))
+		decisions_5 = self.parser.get_decisions_from_txt(txt_5, len(dec_summaries_5))
 
-		rough_paorg_respa_sections_5 = self.parser.get_rough_respas_of_organization_units_from_pres_decree_txt(txt_5)
-		print(rough_paorg_respa_sections_5)
+		self.assertTrue(len(decisions_1) == 1);
+		self.assertTrue(len(decisions_2) == 1); 
+		self.assertTrue(len(decisions_3) == 1); 
+		self.assertTrue(len(decisions_4) == 1);
+		self.assertTrue(len(decisions_5) == 1);
+
+		# Convert any dict to list
+		if isinstance(decisions_1, dict): decisions_1 = list(decisions_1.values())
+		if isinstance(decisions_2, dict): decisions_2 = list(decisions_2.values())
+		if isinstance(decisions_3, dict): decisions_3 = list(decisions_3.values())
+		if isinstance(decisions_4, dict): decisions_4 = list(decisions_4.values())
+		if isinstance(decisions_5, dict): decisions_5 = list(decisions_5.values())
+
+		articles_1 = self.parser.get_pres_decree_articles_from_txt(decisions_1[0])
+		articles_2 = self.parser.get_pres_decree_articles_from_txt(decisions_2[0])
+		articles_3 = self.parser.get_pres_decree_articles_from_txt(decisions_3[0])
+		articles_4 = self.parser.get_pres_decree_articles_from_txt(decisions_4[0])
+		articles_5 = self.parser.get_pres_decree_articles_from_txt(decisions_5[0])
+
+		# Convert any dict to list
+		if isinstance(articles_1, dict): articles_1 = list(articles_1.values())
+		if isinstance(articles_2, dict): articles_2 = list(articles_2.values())
+		if isinstance(articles_3, dict): articles_3 = list(articles_3.values())
+		if isinstance(articles_4, dict): articles_4 = list(articles_4.values())
+		if isinstance(articles_5, dict): articles_5 = list(articles_5.values())
+
+		rough_paorg_respa_sections_in_articles_1 = [self.parser.get_rough_respas_of_organization_units_from_pres_decree_txt(article)
+													for article in articles_1]
+		rough_paorg_respa_sections_in_articles_1 = list(filter(None, rough_paorg_respa_sections_in_articles_1))
+
+		rough_paorg_respa_sections_1 = [rough_paorg_respa_section 
+										for rough_paorg_respa_sections_per_article in rough_paorg_respa_sections_in_articles_1
+										for rough_paorg_respa_section in rough_paorg_respa_sections_per_article]
+		print(len(rough_paorg_respa_sections_1))
+
+		# rough_paorg_respa_sections_in_articles_2 = [self.parser.get_rough_respas_of_organization_units_from_pres_decree_txt(article)
+		# 											for article in articles_2]
+		# rough_paorg_respa_sections_in_articles_2 = list(filter(None, rough_paorg_respa_sections_in_articles_2))
+
+		# rough_paorg_respa_sections_2 = [rough_paorg_respa_section 
+		# 								for rough_paorg_respa_sections_per_article in rough_paorg_respa_sections_in_articles_2
+		# 								for rough_paorg_respa_section in rough_paorg_respa_sections_per_article]
+		# print(rough_paorg_respa_sections_2)
+		
+		
 
 
 if __name__ == '__main__':
