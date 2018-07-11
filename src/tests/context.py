6d4cdc6a4ee11a8d0e545b4ab0a8698625f327d7
ChrisKK <ckarageorgkaneen@gmail.com>
2018-07-11 16:40:40 +0300

    Mv extra methods of test modules here

--- src/tests/context.py
+++ src/tests/context.py
@@ -33,8 +33,6 @@ class Context(unittest.TestCase):
 		self.analyzer = main.analyzer.Analyzer()
 		self.helper = util.helper.Helper()
 
-		print("all ok")
-
 	def tearDown(self): 
 		# rmtree('..' + self.test_txts_dir)
 		pass
@@ -42,4 +40,64 @@ class Context(unittest.TestCase):
 	def get_txt(self, file_name, pdf_path=test_pdfs_dir+"/Decision_Issues/", txt_path=test_txts_dir+"/"):
 			return self.parser.get_simple_pdf_text('..' + pdf_path + file_name + '.pdf', 
 												   '..' + txt_path + file_name + '.txt')
-		
+	def analyze_article(self, artcl):
+			return self.analyzer.get_respa_kw_analysis_of_paorg_pres_decree_article(artcl)
+
+	def get_n_gram_analysis_data_vectors(self, articles):
+		articles_data_vectors = []
+		for artcl in articles: 
+			artcl_analysis_data = self.analyze_article(artcl)
+			print(artcl.partition('\n')[0], ':', artcl_analysis_data)
+			bigram_data_dict, quadgram_data_dict = artcl_analysis_data['bigram_analysis'],\
+													   artcl_analysis_data['quadgram_analysis']
+			bigram_data_vector, quadgram_data_vector = [bigram_data_dict[kw_pair] for kw_pair in bigram_data_dict.keys()],\
+			 										   [quadgram_data_dict[kw_pair] for kw_pair in quadgram_data_dict.keys()]
+
+			custom_n_gram_data_vector = bigram_data_vector + quadgram_data_vector
+			articles_data_vectors.append(custom_n_gram_data_vector)
+			
+		return articles_data_vectors
+
+	def get_custom_n_gram_analysis_data_vector(self, articles):
+		articles_custom_data_vectors = []
+		for artcl in articles: 
+			artcl_analysis_data = self.analyze_article(artcl)
+			
+			custom_condition = artcl_analysis_data['bigram_analysis'][('αρμόδι', 'για')] == 0 and\
+							   artcl_analysis_data['bigram_analysis'][('ρμοδιότητες', 'τ')] == 0 and\
+							   (sum(artcl_analysis_data['bigram_analysis'].values()) == 1 or\
+							   	sum(artcl_analysis_data['quadgram_analysis'].values()) == 1)
+
+			if custom_condition:
+				print(artcl.partition('\n')[0], ':', artcl_analysis_data)
+
+				bigram_data_dict, quadgram_data_dict = artcl_analysis_data['bigram_analysis'],\
+													   artcl_analysis_data['quadgram_analysis']
+				bigram_data_vector, quadgram_data_vector = [bigram_data_dict[kw_pair] for kw_pair in bigram_data_dict.keys()],\
+				 										   [quadgram_data_dict[kw_pair] for kw_pair in quadgram_data_dict.keys()]
+
+				custom_n_gram_data_vector = bigram_data_vector + quadgram_data_vector
+				articles_custom_data_vectors.append(custom_n_gram_data_vector)
+				
+		return articles_custom_data_vectors
+
+	def analyze_issue(self, issue_articles):
+				analysis_data_sums = {'bigram_analysis_sum': {('αρμόδι', 'για'): 0, ('εύθυν', 'για'): 0, ('ευθύνη', 'για'): 0, 
+															  ('αρμοδιότητ', 'ακόλουθ'): 0, ("αρμοδιότητ", "ακόλουθ"): 0, 
+															  ("αρμοδιότητ", "μεταξύ"): 0, ("ρμοδιότητες", "τ"): 0},
+									  'quadgram_analysis_sum': {('αρμοδιότητ', 'έχει'): 0, ('αρμοδιότητ', 'εξής'): 0, 
+									  								('αρμοδιότητ', 'είναι'): 0} 
+								 	  }
+
+				for artcl in issue_articles:
+					respa_occurences_in_artcl = self.analyzer.get_respa_kw_analysis_of_paorg_pres_decree_article(artcl)
+					# Bigram data
+					for key in analysis_data_sums['bigram_analysis_sum'].keys():
+						analysis_data_sums['bigram_analysis_sum'][key] +=\
+						respa_occurences_in_artcl['bigram_analysis'][key]
+					# Quatrogram data
+					for key in analysis_data_sums['quadgram_analysis_sum'].keys():
+						analysis_data_sums['quadgram_analysis_sum'][key] +=\
+						respa_occurences_in_artcl['quadgram_analysis'][key]
+				
+				return analysis_data_sums				
