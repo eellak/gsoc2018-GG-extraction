fda85207200245d66202dbc5c4c6bf5e948340c4
ChrisKK <ckarageorgkaneen@gmail.com>
2018-07-18 22:44:43 +0300

    Fill fit() & cosine_similarity()

--- src/main/classifier.py
+++ src/main/classifier.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 from numpy import mean
+from math import sqrt
 from util.helper import Helper
 
 class RespAClassifier(object):
@@ -54,14 +55,39 @@ class ParagraphRespAClassifier(object):
 	def __init__(self, training_data_files):
 		super(ParagraphRespAClassifier, self).__init__()
 		self.training_data_files = training_data_files
-		self.respa_training_data = Helper.load_pickle_file(training_data_files['respa'])
-		self.non_respa_training_data = Helper.load_pickle_file(training_data_files['non_respa'])
+		self.training_data = {  
+								'non_respa': Helper.load_pickle_file(training_data_files['non_respa']),
+								'respa': Helper.load_pickle_file(training_data_files['respa']) 
+							  }
 
 	def fit(self, paragraph, is_respa):
-		pass
+		words = Helper.get_clean_words(paragraph)[:20]
+		word_bigrams = Helper.get_word_n_grams(words, 2)
+		
+		appropriate_train_data_key = list(self.training_data)[is_respa]
+		# Fit into training data
+		for bigram in word_bigrams: 
+			self.training_data[appropriate_train_data_key][bigram] += 1
+
+		# And rewrite pickle file
+		appropriate_data_file_key = list(self.training_data_files)[is_respa]
+		Helper.write_to_pickle_file(self.training_data, self.training_data_files[appropriate_data_file_key])
 
 	def test(self, paragraph):
 		pass
 
 	def cosine_similarity(self, dict1, dict2):
-		pass
+		numer = 0
+		den_a = 0
+		
+		for key1,val1 in dic1:
+			numer += val1 * dic2.get(key1, 0.0)
+			den_a += va1 * val1
+		den_b = 0
+		
+		for val2 in dic2.values():
+			den_b += val2 * val2
+		
+		return numer/sqrt(den_a * den_b)
+		 
+   
